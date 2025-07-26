a97cd47bcff70c1e9a571432b1fe54ffe137a984
GHTan2002 <guohuang2002@gmail.com>
2025-07-26 20:01:26 +0800

    player animation + item update

--- scenes/hub/testing_body.gd
+++ scenes/hub/testing_body.gd
@@ -1,18 +1,46 @@
 extends CharacterBody2D
 
-# 玩家移动速度（单位：像素/秒）
+# run animation
+@onready var anim_sprite: AnimatedSprite2D = $AnimatedSprite2D
+# player movement base speed（frame/sec）
 @export var move_speed := 100
+# declare if player ded
+var is_dead: bool = false
 
 func _physics_process(delta):
-	var input_vector = Vector2.ZERO
-
+	# run dead animation
+	if is_dead:
+		if anim_sprite.animation != "died":
+			anim_sprite.play("died")
+		velocity = Vector2.ZERO
+		return
+	
+	# Character facing
 	look_at(get_global_mouse_position())
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
- 
-	# 如果有输入就归一化方向，避免对角线更快
-	if input_vector != Vector2.ZERO:
+	
+	# Get inputs
+	var input_vector := Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+	
+	# If shift is pressed, slow down (stealth mode)
+	var speed_multiplier := 1.0
+	if Input.is_action_pressed("stealth"):
+		speed_multiplier = 0.33  # Slowdown multiplier
+	
+	# Move and animate
+	if input_vector.length() > 0:
+		# normalized movement speed
 		input_vector = input_vector.normalized()
-
-	velocity = input_vector * move_speed
+		# Calculate and apply movement
+		velocity = input_vector * move_speed * speed_multiplier
+		
+		# Run moving animation
+		if anim_sprite.animation != "move":
+			anim_sprite.play("move")
+	else:
+		# run idle animation when vector = 0 (idle)
+		velocity = Vector2.ZERO
+		anim_sprite.play("idle")
 	move_and_slide()
