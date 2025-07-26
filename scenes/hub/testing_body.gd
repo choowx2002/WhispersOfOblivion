7c8b22817ec632342121272f036979594da26589
choowx2002 <85860769+choowx2002@users.noreply.github.com>
2025-07-26 13:26:58 +0800

    hub update

---
+++ scenes/hub/testing_body.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+# 玩家移动速度（单位：像素/秒）
+@export var move_speed := 100
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+
+	# 如果有输入就归一化方向，避免对角线更快
+	if input_vector != Vector2.ZERO:
+		input_vector = input_vector.normalized()
+
+	velocity = input_vector * move_speed
+	move_and_slide()
