9ed48d13d05c2c062b43ea479c8e1c7a052556ce
GHTan2002 <guohuang2002@gmail.com>
2025-07-28 22:04:07 +0800

    fixed player drift issues, change player facing to follow moving direction

--- scenes/Player/testing_body.gd
+++ scenes/Player/testing_body.gd
@@ -15,14 +15,15 @@ func _physics_process(delta):
 		velocity = Vector2.ZERO
 		return
 	
-	# Character facing
-	look_at(get_global_mouse_position())
-	
 	# Get inputs
 	var input_vector := Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
+	
+	# Character facing
+	if input_vector.length() > 0:
+		rotation = input_vector.angle()
 	
 	# If shift is pressed, slow down (stealth mode)
 	var speed_multiplier := 1.0
@@ -34,13 +35,13 @@ func _physics_process(delta):
 		input_vector = input_vector.normalized() # normalized movement speed
 		velocity = input_vector * move_speed * speed_multiplier # Calculate and apply movement
 		
-		## Run moving animation
-		##if anim_sprite.animation != "move":
-			##anim_sprite.play("move")
-	#else:
-		## run idle animation when vector = 0 (idle)
-		#velocity = Vector2.ZERO
-		#anim_sprite.play("idle")
+		# Run moving animation
+		if anim_sprite.animation != "move":
+			anim_sprite.play("move")
+	else:
+		# run idle animation when vector = 0 (idle)
+		velocity = Vector2.ZERO
+		anim_sprite.play("idle")
 	move_and_slide()
 
 func _on_hurtbox_area_entered(area: Area2D) -> void:
