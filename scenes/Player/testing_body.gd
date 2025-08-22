fd66fe782869a4ad03a9a4a9c1e2cd8dd62f1002
TEEYUCHEN <teeyuchen1013@1utar.my>
2025-08-23 01:58:31 +0800

    Create a sample of ui for health bar, there is some bug after died the debug will terminate. And other features like deduction of health bar

--- scenes/Player/testing_body.gd
+++ scenes/Player/testing_body.gd
@@ -1,10 +1,20 @@
 extends CharacterBody2D
 
+signal healthChanged
+
 var anim_sprite: AnimatedSprite2D # run animation
 @export var move_speed := 100 # player movement base speed（frame/sec）
 var is_dead: bool = false # declare if player ded
 
+@export var maxHealth: float = 3.0 # set maximum health to 3 unit
+var currentHealth: float = maxHealth # current heath status
+@onready var heartsContainer = $HeartBar/HeartContainer #read the function in path
+
+
 func _ready():
+	heartsContainer.setMaxHearts(maxHealth) # show heart ui
+	heartsContainer.updateHearts(currentHealth) # update the current heart
+	healthChanged.connect(heartsContainer.updateHearts)
 	anim_sprite = get_node("AnimatedSprite2D")
 	#connect("body_entered", Callable(self, "touch_enemy"))
 
@@ -50,10 +60,28 @@ func _on_hurtbox_area_entered(area: Area2D) -> void:
 	if is_dead:
 		return
 	if area.get_parent().is_in_group("Enemy"):  # check if enemy parent is in group
-		die()
+		currentHealth -= 1.0
+		healthChanged.emit(currentHealth)
+		if currentHealth == 0:
+			die()
 
 func die() -> void:
 	is_dead = true
+	visible = false
+	set_physics_process(false)
 	velocity = Vector2.ZERO
 	anim_sprite.play("died")
 	print("Player died.")
+	await get_tree().create_timer(1.0).timeout
+	respawn()
+	
+func respawn():
+	maxHealth += 1
+	if maxHealth >= 100:
+		return
+		
+	currentHealth = maxHealth
+	visible = true
+	set_physics_process(true)
+	is_dead = false
+	healthChanged.emit(currentHealth)
