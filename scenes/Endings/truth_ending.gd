17c172c67e18499485ce2c870ed76320133351b9
GHTan2002 <guohuang2002@gmail.com>
2025-09-16 15:48:30 +0800

    ending done

--- scenes/Endings/truth_ending.gd
+++ scenes/Endings/truth_ending.gd
@@ -6,15 +6,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	teddy.visible = false
 	label.text = ""
+	await get_tree().create_timer(5).timeout
 	anim.play("truth_sequence")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func _on_anim_finished(anim_name: String) -> void:
-	if anim_name == "truth_sequence":
-		# Fade to morning light, then wake-up scene
-		get_tree().change_scene_to_file("res://scenes/endings/wake_up_truth.tscn")
--- scenes/main/Main_transition.gd
+++ scenes/main/Main_transition.gd
@@ -4,11 +4,7 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var gs = get_node("/root/GameState")
-	gs.connect("truth_ending_triggered", Callable(self, "_on_truth_ending"))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func _on_truth_ending() -> void:
-	get_tree().change_scene_to_file("res://scenes/Endings/truth_ending.tscn")
