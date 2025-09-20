b979d1d6ebcd611d63dbafae4295ae2656c8d73e
GHTan2002 <guohuang2002@gmail.com>
2025-09-20 08:41:08 +0800

    Revert "Initial commit linking to existing repo"
    This reverts commit 7793bd5e6c57cd8c0d07d31b4805bfe6cd2c12d4.

--- autoload/global_setting.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-var volume: float = 1.0        
-var brightness: float = 0.2    
--- scenes/ui/sanity_gui.gd
+++
@@ -1,6 +0,0 @@
-extends Panel
-
-@onready var sprite: Sprite2D = $Sprite2D
-
-func update(_filled: bool):
-	sprite.frame = 0
